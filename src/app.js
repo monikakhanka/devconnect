94d6e71f700efaf3fe2fee9f419b549f353c119a
monika <monikakhanka@gmail.com>
2024-11-19 13:15:52 +0530

    cors origin url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000;
 
 app.use(
     cors({
-        origin: "https://devconnect-9vvg.onrender.com",
+        origin: "https://devconnect-web.onrender.com",
         credentials: true, 
     }
 ));
